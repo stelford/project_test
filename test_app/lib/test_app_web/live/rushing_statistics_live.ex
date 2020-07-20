8478ae6dd44b96deb2939c1787bd766ba76eb379
Stef T <stef@ummon.com>
2020-07-20 15:24:06 -0400

    basic pagination of the liveview..
    and I really do mean basic. It currently only does the 'jump to page X' on the view

--- test_app/lib/test_app_web/live/rushing_statistics_live.ex
+++ test_app/lib/test_app_web/live/rushing_statistics_live.ex
@@ -5,12 +5,25 @@ defmodule TestAppWeb.RushingStatisticsLive do
   alias TestApp.Football.Player.RushingStatistic, as: RushingStatistic
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, statistics: nil, order_by: nil)}
+    {:ok, assign(socket, statistics: nil, order_by: :player_name, current_page: 1, page_size: 25)}
   end
 
   def render(assigns) do
     ~L"""
     <h1>LiveView is alive</h1>
+    <% all_pages = number_of_pages(assigns) %>
+
+    <form phx-change="select-page">
+      Page
+      <select name="page">
+        <%= for page <- all_pages do %>
+          <option value="<%= page %>" <%= page == assigns.current_page && "selected" || "" %>>
+            <%= page %>
+          </option>
+        <% end %>
+      </select>
+    </form>
+
     <table>
       <thead>
         <th>
@@ -25,14 +38,39 @@ defmodule TestAppWeb.RushingStatisticsLive do
         <% end %>
       </tbody>
     </table>
+
+    <form phx-change="select-page">
+      Page
+      <select name="page">
+        <%= for page <- all_pages do %>
+          <option value="<%= page %>" <%= page == assigns.current_page && "selected" || "" %>>
+            <%= page %>
+          </option>
+        <% end %>
+      </select>
+    </form>
     """
   end
 
+  def handle_event("select-page", %{"page" => page}, socket) do
+    {:noreply, assign(socket, current_page: String.to_integer(page))}
+  end
 
-  defp statistics(%{statistics: nil, order_by: nil}) do
+  defp statistics(%{statistics: nil, order_by: order_by, current_page: current_page, page_size: page_size}) do
+    IO.puts(order_by)
     from(a in RushingStatistic,
-         order_by: a.player_name)
+      order_by: ^order_by,
+      offset: ^((current_page-1)*page_size),
+      limit: ^page_size
+    )
     |> TestApp.Repo.all()
+  end
+
+  defp number_of_pages(%{page_size: page_size}) do
+    count_result = from(a in RushingStatistic, select: count(a.id))
+                   |> TestApp.Repo.one()
+    count_result = trunc(Float.ceil(count_result / page_size))
+    Enum.to_list(1..count_result)
   end
 
 end
