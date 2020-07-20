a73d78edf3f0dbb0485b2502a55ac0403d96df8e
Stef T <stef@ummon.com>
2020-07-19 20:03:15 -0400

    quickly flinging in some data to the liveview
    mostly to prove to myself that this is actually working, but also it's a stopping point for the night

--- test_app/lib/test_app_web/live/rushing_statistics_live.ex
+++ test_app/lib/test_app_web/live/rushing_statistics_live.ex
@@ -1,13 +1,38 @@
 defmodule TestAppWeb.RushingStatisticsLive do
   use Phoenix.LiveView, layout: {TestAppWeb.LayoutView, "live.html"}
 
+  import Ecto.Query
+  alias TestApp.Football.Player.RushingStatistic, as: RushingStatistic
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, statistics: nil, order_by: nil)}
   end
 
   def render(assigns) do
     ~L"""
-    <h1>LiveView is awesome!</h1>
+    <h1>LiveView is alive</h1>
+    <table>
+      <thead>
+        <th>
+          Name
+        </th>
+      </thead>
+      <tbody>
+        <%= for stat <- statistics(assigns) do %>
+          <tr>
+            <td><%= stat.player_name %></td>
+          </tr>
+        <% end %>
+      </tbody>
+    </table>
     """
   end
+
+
+  defp statistics(%{statistics: nil, order_by: nil}) do
+    from(a in RushingStatistic,
+         order_by: a.player_name)
+    |> TestApp.Repo.all()
+  end
+
 end
