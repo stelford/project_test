cb8891a79b87f763fc5377883640c99aca87c041
Stef T <stef@ummon.com>
2020-07-21 20:08:37 -0400

    Add in other columns now and allow sorting
    on any column.. either ascending or descending. I am not a HUGE fan of the 'asc'/'desc' suffix
    that appears on the active column.. but to be honest, I didn't want to spend ages looking for
    the right icons/assets for visuals. I know, I know.

--- test_app/lib/test_app_web/live/rushing_statistics_live.ex
+++ test_app/lib/test_app_web/live/rushing_statistics_live.ex
@@ -6,7 +6,7 @@ defmodule TestAppWeb.RushingStatisticsLive do
   alias TestApp.Football.Player.RushingStatistic, as: RushingStatistic
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, statistics: nil, order_by: :player_name, current_page: 1, page_size: 25)}
+    {:ok, assign(socket, order_by: :player_name, asc_or_desc: :asc, current_page: 1, page_size: 25)}
   end
 
   def render(assigns) do
@@ -32,11 +32,50 @@ defmodule TestAppWeb.RushingStatisticsLive do
 
     <table>
       <thead>
-        <th phx-click="order-by" phx-value-order_by="player_name">
-          Name
+        <th phx-click="order-by" phx-value-order_by="player_name" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :player_name do 'column-active'; else ''; end %>">
+          Name <%= if assigns.order_by == :player_name do assigns.asc_or_desc; else ''; end %>
         </th>
-        <th phx-click="order-by" phx-value-order_by="team">
-          Team
+        <th phx-click="order-by" phx-value-order_by="team" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :team do 'column-active'; else ''; end %>">
+          Team <%= if assigns.order_by == :team do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="player_position" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :player_position do 'column-active'; else ''; end %>">
+          Position <%= if assigns.order_by == :player_position do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="attempts" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :attempts do 'column-active'; else ''; end %>">
+          Attempts <%= if assigns.order_by == :attempts do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="attempts_per_game" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :attempts_per_game do 'column-active'; else ''; end %>">
+          Attempts Per Game <%= if assigns.order_by == :attempts_per_game do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="yards_per_game" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :yards_per_game do 'column-active'; else ''; end %>">
+          Yards Per Game <%= if assigns.order_by == :yards_per_game do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="average_yards" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :average_yards do 'column-active'; else ''; end %>">
+          Average Yards <%= if assigns.order_by == :average_yards do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="total_yards" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :total_yards do 'column-active'; else ''; end %>">
+          Total Yards <%= if assigns.order_by == :total_yards do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="touchdowns" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :touchdowns do 'column-active'; else ''; end %>">
+          Touchdowns <%= if assigns.order_by == :touchdowns do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="longest_rush" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :longest_rush do 'column-active'; else ''; end %>">
+          Longest Rush <%= if assigns.order_by == :longest_rush do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="first_downs" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :first_downs do 'column-active'; else ''; end %>">
+          First Downs <%= if assigns.order_by == :first_downs do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="first_downs_pct" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :first_downs_pct do 'column-active'; else ''; end %>">
+          First Downs Pct <%= if assigns.order_by == :first_downs_pct do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="twenty_yards_plus" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :twenty_yards_plus do 'column-active'; else ''; end %>">
+          Twenty Yards Plus <%= if assigns.order_by == :twenty_yards_plus do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="fourty_yards_plus" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :fourty_yards_plus do 'column-active'; else ''; end %>">
+          Fourty Yards Plus <%= if assigns.order_by == :fourty_yards_plus do assigns.asc_or_desc; else ''; end %>
+        </th>
+        <th phx-click="order-by" phx-value-order_by="fumbles" phx-value-asc_or_desc="<%= assigns.asc_or_desc %>" class="<%= if assigns.order_by == :fumbles do 'column-active'; else ''; end %>">
+          Fumbles <%= if assigns.order_by == :fumbles do assigns.asc_or_desc; else ''; end %>
         </th>
       </thead>
       <tbody>
@@ -44,6 +83,19 @@ defmodule TestAppWeb.RushingStatisticsLive do
           <tr>
             <td><%= stat.player_name %></td>
             <td><%= stat.team %></td>
+            <td><%= stat.player_position %></td>
+            <td><%= stat.attempts %></td>
+            <td><%= stat.attempts_per_game %></td>
+            <td><%= stat.yards_per_game %></td>
+            <td><%= stat.average_yards %></td>
+            <td><%= stat.total_yards %></td>
+            <td><%= stat.touchdowns %></td>
+            <td><%= stat.longest_rush %></td>
+            <td><%= stat.first_downs %></td>
+            <td><%= stat.first_downs_pct %></td>
+            <td><%= stat.twenty_yards_plus %></td>
+            <td><%= stat.fourty_yards_plus %></td>
+            <td><%= stat.fumbles %></td>
           </tr>
         <% end %>
       </tbody>
@@ -66,14 +118,27 @@ defmodule TestAppWeb.RushingStatisticsLive do
     {:noreply, assign(socket, current_page: String.to_integer(page))}
   end
 
-  def handle_event("order-by", %{"order_by" => order_by}, socket) do
-    {:noreply, assign(socket, order_by: String.to_atom(order_by))}
+  def handle_event("order-by", %{"asc_or_desc" => "asc", "order_by" => order_by}, socket) do
+    IO.inspect(socket)
+    {:noreply, assign(socket, %{order_by: String.to_atom(order_by), asc_or_desc: :desc})}
   end
 
-  defp statistics(%{statistics: nil, order_by: order_by, current_page: current_page, page_size: page_size}) do
-    IO.puts(order_by)
+  def handle_event("order-by", %{"asc_or_desc" => "desc", "order_by" => order_by}, socket) do
+    {:noreply, assign(socket, %{order_by: String.to_atom(order_by), asc_or_desc: :asc})}
+  end
+
+  defp statistics(%{order_by: order_by, asc_or_desc: :asc, current_page: current_page, page_size: page_size}) do
     from(a in RushingStatistic,
-      order_by: ^order_by,
+      order_by: [asc: ^order_by],
+      offset: ^((current_page-1)*page_size),
+      limit: ^page_size
+    )
+    |> TestApp.Repo.all()
+  end
+
+  defp statistics(%{order_by: order_by, asc_or_desc: :desc, current_page: current_page, page_size: page_size}) do
+    from(a in RushingStatistic,
+      order_by: [desc: ^order_by],
       offset: ^((current_page-1)*page_size),
       limit: ^page_size
     )
